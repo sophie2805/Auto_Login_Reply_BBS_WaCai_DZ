c766af826865b392718c09a0bda36e3a050f680e
Sophie <sophie@163.com>
2015-06-28 19:55:49 +0800

    add command line run parameters and get the absolute executing path to create or modify log file

--- Auto_Login_Reply.py
+++ Auto_Login_Reply.py
@@ -7,14 +7,27 @@ import re
 import time
 import requests
 from bs4 import BeautifulSoup
+import os.path
+import sys
 
+'''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+python Auto_Login_Reply.py user/pwd
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
+
+args = sys.argv[1]
+#print args
+username = args[0:args.find('/')]
+pwd = args[args.find('/')+1:len(args)]
+#print username , pwd
 
 '''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 using log_list[] to log the whole process
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
+
+#print os.path.abspath('.')
 log_list = []
 log_list.append('+++++++++++++++++++++++++++++++++++++++++++++\n')
-log_list.append('+++++挖财签到有礼'+(time.strftime("%m.%d %T"))+' 每天签到得铜钱+++++\n')
+log_list.append('++++挖财签到有礼'+(time.strftime("%m.%d %T"))+' 每天签到得铜钱++++\n')
 log_list.append('+++++++++++++++++++++++++++++++++++++++++++++\n')
 
 s = requests.Session()
@@ -32,14 +45,14 @@ post login request to this URL, observed in Firebug
 login_url = 'https://www.wacai.com/user/user!login.action?cmd=null'
 
 login_post_data ={
-    'user.account':'***',
-    'user.pwd':'***'
+    'user.account':username,
+    'user.pwd':pwd
 }
 
 try:
     login_r = s.post(login_url,login_post_data)
 except Exception,e:
-    log_list.append('Login Exception: '+ e + time.strftime("%m.%d %T") + '.\n')
+    log_list.append(time.strftime("%m.%d %T") + '--Login Exception: '+ e + '.\n')
 #print login_r.content
 
 '''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -59,7 +72,7 @@ s.get(src2)
 base_url = 'http://bbs.wacai.com/'
 homepage_r = s.get(base_url)
 if '我的挖财' in homepage_r.content:
-    log_list.append('Successfully login '+time.strftime("%m.%d %T")+'.\n')
+    log_list.append(time.strftime("%m.%d %T") + '--Successfully login.\n')
 
 '''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 find the checkin forum URL and ID, which is used as fid parameter in the reply post URL
@@ -73,7 +86,7 @@ checkin_forum_url = base_url+ checkin_postfix
 forum_id = checkin_postfix[checkin_postfix.find('-')+1:checkin_postfix.rfind('-')]
 
 if forum_id != '':
-    log_list.append('Successfully find the checkin forum ID '+time.strftime("%m.%d %T")+'.\n')
+    log_list.append(time.strftime("%m.%d %T") + '--Successfully find the checkin forum ID.\n')
 
 '''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 get the checkin forum portal page and find today's thread URL and ID, which is used as tid parameter in the reply post URL
@@ -90,7 +103,7 @@ thread_id= thread_postfix[thread_postfix.find('-')+1:thread_postfix.rfind('-')-2
 #print thread_id
 
 if thread_id != '':
-    log_list.append('Successfully find the thread ID '+ time.strftime("%m.%d %T")+'.\n')
+    log_list.append(time.strftime("%m.%d %T") + '--Successfully find the thread ID.\n')
 t = s.get(thread_url)
 
 '''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -107,7 +120,7 @@ result_3 = re.compile(pattern_3).findall(t.content)
 #print result_3
 key = result_3[0][result_3[0].find('>')+1:result_3[0].rfind('<')-1]
 if key != '':
-    log_list.append('Successfully find the key word '+time.strftime("%m.%d %T")+' .\n')
+    log_list.append(time.strftime("%m.%d %T") + '--Successfully find the key word.\n')
 
 '''~~~~~~~
 auto reply
@@ -126,11 +139,11 @@ reply_post_url = 'http://bbs.wacai.com/forum.php?mod=post&action=reply&fid='+for
 try:
     reply_r = s.post(reply_post_url,data=reply_data)
 except Exception,e:
-    log_list.append('Reply exception: '+ e +time.strftime("%m.%d %T")+' .\n' )
+    log_list.append(time.strftime("%m.%d %T") + '--Reply exception: '+ e +'.\n' )
 if '非常感谢，回复发布成功，现在将转入主题页，请稍候……' in reply_r.content:#success
-    log_list.append('successfully auto reply'+time.strftime("%m.%d %T")+' .\n')
+    log_list.append(time.strftime("%m.%d %T") + '--Successfully auto reply.\n')
 else:
-    log_list.append('Fail to reply: '+time.strftime("%m.%d %T")+ reply_r.content + ' .\n')
+    log_list.append(time.strftime("%m.%d %T") + '--Fail to reply: '+ reply_r.content + '.\n')
 
 '''~~~~~~~~~~~~~~
 find my WaCai URL
@@ -139,7 +152,7 @@ pattern_4 = '<.+访问我的空间.+</a>'
 p_4 = re.compile(pattern_4)
 soup = BeautifulSoup(p_4.findall(t.content)[0])
 if soup.a['href'] != '':
-    log_list.append('Successfully find my WaCai link '+time.strftime("%m.%d %T")+' .\n' )
+    log_list.append(time.strftime("%m.%d %T") + '--Successfully find my WaCai link.\n' )
 mywacai_url = base_url + soup.a['href']
 mywacai_page = s.get(mywacai_url)
 
@@ -150,7 +163,7 @@ pattern_5 = '<.+个人资料</a>'
 p_5 = re.compile(pattern_5)
 soup = BeautifulSoup(p_5.findall(mywacai_page.content)[0])
 if soup.a['href'] != '':
-    log_list.append('Successfully find my info link '+time.strftime("%m.%d %T")+' .\n' )
+    log_list.append(time.strftime("%m.%d %T") + '--Successfully find my info link.\n' )
 myinfo_url = base_url+ soup.a['href']
 myinfo_page = s.get(myinfo_url)
 
@@ -162,24 +175,27 @@ p_6 = re.compile(pattern_6)
 coin = p_6.findall(myinfo_page.content)[0]
 coin = coin[coin.find('</em>')+5:coin.find('</li>')]
 if int(coin.strip()) != 0:
-    log_list.append('Successfully get my coin amount '+time.strftime("%m.%d %T")+': %s .\n'% int(coin.strip()))
+    log_list.append(time.strftime("%m.%d %T") + '--Successfully get my coin amount: %s.\n'% int(coin.strip()))
 log_list.append('\n')
 
 '''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+if log.txt does not exist under current executing path, create it.
 write log, if the log file is larger than 100 lines, delete all then write from beginning
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'''
-f = open('/Users/Sophie/PycharmProjects/Auto_Login_Reply_BBS_WaCai/log.txt','r')
-try:
-    count = len(f.readlines())
-finally:
+
+file = os.path.abspath('.')+'/log.txt'#get the absolute path of .py executing
+if not os.path.isfile(file):#not exist, create a new one
+    f = open(file,'w')
     f.close()
-if count >= 100:
-    f = open('/Users/Sophie/PycharmProjects/Auto_Login_Reply_BBS_WaCai/log.txt','w')
+
+if os.path.getsize(file)/1024 > 1024:#larger than 1MB
+    f = open(file,'w')
     try:
         f.write('')
     finally:
         f.close()
-f = open('/Users/Sophie/PycharmProjects/Auto_Login_Reply_BBS_WaCai/log.txt','a')#append
+
+f = open(file,'a')#append
 try:
     f.writelines(log_list)
 finally:
